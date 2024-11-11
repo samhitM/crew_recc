642120664d581ad8a7421b48c0179f9c394c6857
Ashish Chokhani <ashish.chokhani@students.iiit.ac.in>
2024-11-11 09:50:54 +0000

    Update config.py

--- Gamer_Recommendation/core/config.py
+++ Gamer_Recommendation/core/config.py
@@ -8,9 +8,9 @@ DATABASE = 'crewdb'
 DB_USER = 'admin_crew'
 DB_PASSWORD = 'xV/nI2+=uOI&KL1P'
 
-# Secret key for token verification
-SECRET_KEY = "1c75f472b1e52c582c8ed3f4d88af9c0137f9a2eeeb1d63e97ecedd1be8f1a3c"
-VALID_SECRET_KEY = '7irNPR6kOia'  # Add valid secret key as a variable for easy management
+# # Secret key for token verification
+# SECRET_KEY = "1c75f472b1e52c582c8ed3f4d88af9c0137f9a2eeeb1d63e97ecedd1be8f1a3c"
+# VALID_SECRET_KEY = '7irNPR6kOia'  # Add valid secret key as a variable for easy management
 
 def get_secret(secret_name):
     """Fetches a secret's value from Google Secret Manager."""
@@ -20,6 +20,6 @@ def get_secret(secret_name):
     response = client.access_secret_version(request={"name": secret_path})
     return response.payload.data.decode("UTF-8")
 
-# # Secret keys for token verification, retrieved securely
-# SECRET_KEY = get_secret("SECRET_KEY")
-# VALID_SECRET_KEY = get_secret("VALID_SECRET_KEY")
+# Secret keys for token verification, retrieved securely
+SECRET_KEY = get_secret("SECRET_KEY")
+VALID_SECRET_KEY = get_secret("VALID_SECRET_KEY")
